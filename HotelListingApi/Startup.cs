97191c78aa24191c3ea4d9efe7b7d3e5f5073e92
Suraj Kumar <postbox.suraj@outlook.com>
2021-12-06 12:29:01 +0530

    Added Country and Hotel Get Methods. Also fixed Unit Of Work DI reference.

--- HotelListingApi/Startup.cs
+++ HotelListingApi/Startup.cs
@@ -1,5 +1,7 @@
 using HotelListingApi.Configurations;
+using HotelListingApi.IRepository;
 using HotelListingApi.Models;
+using HotelListingApi.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -41,12 +43,15 @@ namespace HotelListingApi
 
             services.AddAutoMapper(typeof(MapperIntializer));
 
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListingApi", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op =>
+            op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
